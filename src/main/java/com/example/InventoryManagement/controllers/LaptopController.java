bc542b49b58d0ccb5e82a001dc1ec43073d48500
Nick Englezakis <95222208+nikoengl27@users.noreply.github.com>
2022-04-04 15:39:58 +0100

    Added mapping for tvs

--- src/main/java/com/example/InventoryManagement/controllers/LaptopController.java
+++ src/main/java/com/example/InventoryManagement/controllers/LaptopController.java
@@ -21,7 +21,6 @@ public class LaptopController {
         return new ResponseEntity<>(laptopRepository.findAll(), HttpStatus.OK);
     }
 
-
 }
 
 
--- src/main/java/com/example/InventoryManagement/controllers/TvController.java
+++ src/main/java/com/example/InventoryManagement/controllers/TvController.java
@@ -1,4 +1,20 @@
 package com.example.InventoryManagement.controllers;
 
+import com.example.InventoryManagement.models.Laptop;
+import com.example.InventoryManagement.models.TV;
+import com.example.InventoryManagement.repositories.TvRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 public class TvController {
+
+    TvRepository tvRepository;
+
+    @GetMapping(value = "/tvs")
+    public ResponseEntity<List<TV>> getAllLaptops(){
+        return new ResponseEntity<>(tvRepository.findAll(), HttpStatus.OK);
+    }
 }
