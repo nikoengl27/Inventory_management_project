b7c58900e0c9c0a9107548aaecbd43020e30ccee
Paolaguerralibrero <mpaolamcra@hotmail.com>
2022-04-04 14:58:43 +0100

    test2

--- src/main/java/com/example/InventoryManagement/controllers/LaptopController.java
+++ src/main/java/com/example/InventoryManagement/controllers/LaptopController.java
@@ -1,11 +1,14 @@
 package com.example.InventoryManagement.controllers;
 
 
+import com.example.InventoryManagement.repositories.LaptopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LaptopController {
+
+    LaptopRepository laptopRepository;
 
 
 }
