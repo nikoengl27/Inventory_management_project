113d34e3f2249ea3c6a7cf92d888a4768c2be81c
Andreas Skarpelos <95192235+Skarbel@users.noreply.github.com>
2022-04-05 10:33:58 +0100

    test commit

--- src/main/java/com/example/InventoryManagement/controllers/TvController.java
+++ src/main/java/com/example/InventoryManagement/controllers/TvController.java
@@ -1,26 +1,6 @@
 package com.example.InventoryManagement.controllers;
 
-import com.example.InventoryManagement.models.MobilePhone;
-import com.example.InventoryManagement.models.TV;
-import com.example.InventoryManagement.repositories.TvRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestControlle
 public class TvController {
 
-    TvRepository tvRepository;
-
-    @GetMapping(value = "/tvs")
-    public ResponseEntity<List<TV>> getAllTvs(){
-        return new ResponseEntity<>(tvRepository.findAll(), HttpStatus.OK);
-    }
-
-
-
-
+    //123
 }
