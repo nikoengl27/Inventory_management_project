ddf4c8a11b2b8a6ade9a0d3763ee1bbbe2889d13
Nick Englezakis <95222208+nikoengl27@users.noreply.github.com>
2022-04-05 10:35:36 +0100

    Updated tv controller

--- src/main/java/com/example/InventoryManagement/controllers/TvController.java
+++ src/main/java/com/example/InventoryManagement/controllers/TvController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestControlle
+@RestController
 public class TvController {
 
     TvRepository tvRepository;
