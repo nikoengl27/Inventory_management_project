e9aa3a684793352110f647c694a4498a2b66767c
Nick Englezakis <95222208+nikoengl27@users.noreply.github.com>
2022-04-05 10:33:51 +0100

    Added mapping for phones

--- src/main/java/com/example/InventoryManagement/controllers/MobilePhoneController.java
+++ src/main/java/com/example/InventoryManagement/controllers/MobilePhoneController.java
@@ -1,4 +1,23 @@
 package com.example.InventoryManagement.controllers;
 
+import com.example.InventoryManagement.models.MobilePhone;
+import com.example.InventoryManagement.repositories.MobilePhoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class MobilePhoneController {
+
+    @Autowired
+    MobilePhoneRepository mobilePhoneRepository;
+
+    @GetMapping(value = "/mobilephones")
+    public ResponseEntity<List<MobilePhone>> getAllMobilePhones(){
+        return new ResponseEntity<>(mobilePhoneRepository.findAll(),HttpStatus.OK);
+    }
 }
