00984bdc0d6f27fcb285373c3e8f4260e3e3b1ec
Paolaguerralibrero <mpaolamcra@hotmail.com>
2022-04-05 11:45:45 +0100

    update all routes

--- src/main/java/com/example/InventoryManagement/controllers/LaptopController.java
+++ src/main/java/com/example/InventoryManagement/controllers/LaptopController.java
@@ -2,13 +2,12 @@ package com.example.InventoryManagement.controllers;
 
 
 import com.example.InventoryManagement.models.Laptop;
+import com.example.InventoryManagement.models.TV;
 import com.example.InventoryManagement.repositories.LaptopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,8 +23,20 @@ public class LaptopController {
     }
 
     @GetMapping(value = "/laptops/{id}")
-    public ResponseEntity getLaptop(@PathVariable Long id){
+    public ResponseEntity getLaptopById(@PathVariable Long id){
         return new ResponseEntity(laptopRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value="/laptops")
+    public ResponseEntity<Laptop> postLaptop(@RequestBody Laptop laptop){
+        laptopRepository.save(laptop);
+        return new ResponseEntity<>(laptop, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value="/tvs/{id}")
+    public ResponseEntity<Laptop> updateLaptop(@RequestBody Laptop laptop){
+        laptopRepository.save(laptop);
+        return new ResponseEntity<>(laptop, HttpStatus.OK);
     }
 
 
